74dbc2dafc5c4ec6ce181bae6a9e0e3526b3cdf5
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-06-19 16:01:27 -0400

    added env variables. running into Access Denied error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
+require("dotenv").config();
 const aws = require("aws-sdk");
-//awaiting credentials for config file
-// const config = require("./config.json");
 
 (async function() {
     try {
@@ -8,14 +7,14 @@ const aws = require("aws-sdk");
         aws.config.setPromisesDependency();
 
         aws.config.update({
-            accessKeyId: config.aws.accessKey,
-            secretAccessKey: config.aws.secretKey,
+            accessKeyId: process.env.ACCESS,
+            secretAccessKey: process.env.SECRET,
             region: "us-east-1"
         });
 
         const s3 = new aws.S3();
         const response = await s3.listObjectsV2({
-            Bucket: "cru.org-storylines/index.html"
+            Bucket: "cru.org-storylines"
         }).promise();
 
         console.log(response);
